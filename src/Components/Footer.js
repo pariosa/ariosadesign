6e02fa4e69f446cb209cae6474ef055ab62a8e8e
Peter Ariosa <ariosa@gmail.com>
2022-08-18 15:57:41 -0400

    styling page fixes and year update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,11 @@
-import css from "../css/styles.css";  
 import React from 'react';
 
 export default function Footer() {
   return (
     <footer className="footer">
-        <div className="footer-blurb">
-          © 2021 Peter Ariosa
-        </div>
+      <div className="footer-blurb">
+        © 2022 Peter Ariosa
+      </div>
     </footer>
   )
 } 
